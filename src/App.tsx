1483231d97e6689496e379f3d1232612f1dfa231
João Paulo <jopaulo.as8@gmail.com>
2026-02-25 10:15:16 -0300

    feat: add marquee between hero and about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { MainLayout } from '@/layout/MainLayout';
 import { Hero } from '@/pages/Hero';
+import { Marquee } from '@/components/Marquee';
 
 function App() {
   return (
     <MainLayout>
       <Hero />
+      <Marquee />
     </MainLayout>
   )
 }
